f4ca30a6736ac3f7708ad6c8cec58662068fe615
UMEYukkiy <umeyukkiy1027@gmail.com>
2026-06-26 12:20:20 +0900

    User name added

--- HeadOrTails.java
+++ HeadOrTails.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class HeadOrTails {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     int headsCount = 0;
     int tailsCount = 0;
 
+    System.out.println("Who are you?");
+    String name = sc.nextLine();
+    System.out.println("Hello, " + name + "!");
     System.out.println("Tossing a coin...");
+
     for (int i = 0; i < 3; i++) {
       int coin = (int) (Math.random() * 2);
       if (coin == 0) {
@@ -14,6 +21,8 @@ public class HeadOrTails {
         tailsCount++;
       }
     }
+
     System.out.println("Heads: " + headsCount + " Tails: " + tailsCount);
+    sc.close();
   }
 }
